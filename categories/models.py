8755eb0bbd3d6ccc48b627e83c477f465992e6c4
sabrinara <you@example.com>
2023-12-28 22:52:03 +0600

    Assignment-5-django M-16

--- categories/models.py
+++ categories/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from tasks.models import Task
+# from tasks.models import Task
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    task = models.ManyToManyField(Task)
+    # task = models.ManyToManyField(Task)
 
     def __str__(self):
         return self.name
